6848a9ced0a7fe50249dd8d93134a98cb67f827f
oyejohnson <johnsonoye34@gmail.com>
2019-05-31 09:05:28 +0100

    hope it works again

--- src/rifqoh/storefront/index.js
+++ src/rifqoh/storefront/index.js
@@ -14,6 +14,7 @@ import { connect } from "react-redux";
 class Index extends Component {
   componentDidMount() {
     this.getProducts();
+    document.addEventListener('scroll', this.trackScrolling);
   }
 
   componentDidUpdate(prevProps) {
