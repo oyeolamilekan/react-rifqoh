308824cfb56f5e491b6626fa6be047d333ab1172
oyejohnson <johnsonoye34@gmail.com>
2019-06-19 18:17:49 +0100

    wrote some comments

--- src/rifqoh/utils/utils.js
+++ src/rifqoh/utils/utils.js
@@ -12,13 +12,20 @@ export const jsUcfirst = (string) => {
 }
 
 export function delimitNumbers(str) {
+  /**
+   * Formats the number and puts in comma in the middle
+   * Example: 10000 -> 10,000
+   */
   return (str.replace(/[,;/`]/,'').replace(/[a-zA-Z]/, '').replace(/,/g, '') + "").replace(/\b(\d+)((\.\d+)*)\b/g, function(a, b, c) {
     return (b.charAt(0) > 0 && !(c || ".").lastIndexOf(".") ? b.replace(/(\d)(?=(\d{3})+$)/g, "$1,") : b) + c;
   });
 }
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-  
+  /**
+   * This handles the authenticated users route, redirects to login
+   * page which user can login
+   */
   <Route {...rest} render={(props) => (
     Boolean(localStorage.getItem('is_commerce')) && localStorage.getItem("username") !== "undefined"
       ? <Component {...props} />
@@ -27,7 +34,10 @@ export const PrivateRoute = ({ component: Component, ...rest }) => (
 )
 
 export const UnAuthRoute = ({ component: Component, ...rest }) => (
-  
+  /**
+   * This handles the unauthenticated user route, and redirects the authenticated user from
+   * the login page to commerce product page.
+   */
   <Route {...rest} render={(props) => (
     !Boolean(localStorage.getItem('is_commerce')) && localStorage.getItem("username") === null
       ? <Component {...props} />
