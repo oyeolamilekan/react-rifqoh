7001240b66b2480c0129f7ce4b87c829b85a5b7b
oyejohnson <johnsonoye34@gmail.com>
2019-07-13 09:23:15 +0100

    solved the issue

--- src/rifqoh/config/url.js
+++ src/rifqoh/config/url.js
@@ -3,5 +3,5 @@ let url;
 window.location.hostname.indexOf("localhost") > -1 || window.location.hostname.indexOf("myapp") > -1
   
   ? (url = "http://127.0.0.1:8000")
-  : (url = "https://kirr.xyz/");
+  : (url = "https://kirr.xyz");
 export default url;
